34a1ee27d4a136c140d074ebeec01620b511b343
chad-tung <sven.tung@gmail.com>
2020-02-22 11:04:54 +0000

    Refactors code and files and sets image sizes

--- client/src/Components/rock.js
+++ client/src/Components/rock.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 const Rock = (props) => {
 
   return(
-    <img src="/assets/Boulder-1.png" alt=""/>
+    <img src="/assets/Boulder-1.png" style={{height: "12vh"}} alt=""/>
   )
 
 }
--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import WelcomeContainer from './WelcomeContainer';
 import RpsContainer from './RpsContainer';
