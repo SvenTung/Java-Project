37254eca44704f89a6b89bbe7a467f96c2fa2f82
User <user@Users-MacBook-Pro.local>
2020-02-22 15:38:41 +0000

    game on screen update

--- client/src/Components/images/Scissors.js
+++ client/src/Components/images/Scissors.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Scissors = (props) => {
 
   return(
-    <img src="/assets/scissors.png" style={{height: "10vh"}} alt=""/>
+    <img src="/assets/scissors.png" style={{height: "5vh"}} alt=""/>
   )
 
 }
