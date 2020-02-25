cf12f6daffa2d8e4edf7175cd1b3b7bedb9f2c5f
User <user@Users-MacBook-Pro.local>
2020-02-25 09:21:16 +0000

    added css,media links

--- client/src/containers/WelcomeContainer.js
+++ client/src/containers/WelcomeContainer.js
@@ -22,6 +22,44 @@ const WelcomeContainer = (props) => {
         <button className="yol" type="submit">Add Game</button>
       </form>
       </div>
+      <div class="share-btns">
+      <br/>
+     <a
+       class="share-btn twitter"
+       href="https://twitter.com/share?url=<URL>&text=<TITLE>&via=GregorySchier"
+       target="_blank"
+     >Twitter</a>
+
+     <a
+       class="share-btn facebook"
+       href="https://www.facebook.com/sharer/sharer.php?u=#url<URL>"
+       target="_blank"
+     >Facebook</a>
+
+     <a
+       class="share-btn reddit"
+       href="https://reddit.com/submit?url=<URL>&title=<TITLE>"
+       target="_blank"
+     >Reddit</a>
+
+     <a
+       class="share-btn hackernews"
+       href="https://news.ycombinator.com/submitlink?u=<URL>&t=<TITLE>"
+       target="_blank"
+     >Hacker News</a>
+
+     <a
+       class="share-btn linkedin"
+       href="https://www.linkedin.com/shareArticle?url=<URL>&title=<TITLE>"
+       target="_blank"
+     >LinkedIn</a>
+
+     <a
+       class="share-btn email"
+       href="mailto:?subject=<TITLE>&body=<URL>"
+       target="_blank"
+     >Email</a>
+   </div>
 
     </div>
 
