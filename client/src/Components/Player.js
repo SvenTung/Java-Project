894c56ab33653673d2a51ef0a0f94c1f579ada08
chad-tung <sven.tung@gmail.com>
2020-02-25 09:42:05 +0000

    adds hover css styling

--- client/src/Components/Player.js
+++ client/src/Components/Player.js
@@ -6,17 +6,26 @@ const Player = (props) => {
     <div className={"player"}>
       {props.player.selectedOption === '' ? (
         <>
-          <button onClick={() => props.setPlayerOption("rock")} className={"playerOption"}>
-          <Option option={"rock"}/></button>
-          <button onClick={() => props.setPlayerOption("paper")} className={"playerOption"}>
-          <Option option={"paper"}/></button>
-          <button onClick={() => props.setPlayerOption("scissors")}
-          className={"playerOption"}><Option option={"scissors"}/></button>
+        {props.hoveredOption === 'rock' ? (
+          <button onClick={() => props.setPlayerOption("rock")} className={"hoveredOption"}> <Option option={"rock"}/></button>
+        ) : (
+          <button onClick={() => props.setPlayerOption("rock")} className={"playerOption"}> <Option option={"rock"}/></button>
+        )}
+        {props.hoveredOption === 'paper' ? (
+          <button onClick={() => props.setPlayerOption("paper")} className={"hoveredOption"}> <Option option={"paper"}/></button>
+        ) : (
+          <button onClick={() => props.setPlayerOption("paper")} className={"playerOption"}> <Option option={"paper"}/></button>
+        )}
+        {props.hoveredOption === 'scissors' ? (
+          <button onClick={() => props.setPlayerOption("scissors")} className={"hoveredOption"}> <Option option={"scissors"}/></button>
+        ) : (
+          <button onClick={() => props.setPlayerOption("scissors")} className={"playerOption"}> <Option option={"scissors"}/></button>
+        )}
         </>
       ) : (
-          <Option
-            option={props.player.selectedOption}
-          />
+        <Option
+          option={props.player.selectedOption}
+        />
       )}
     </div>
   )
