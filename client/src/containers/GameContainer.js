277b289c03ca72a1b2f7b03fff5f112e75d4a2a4
chad-tung <sven.tung@gmail.com>
2020-02-26 10:33:19 +0000

    removes game select container

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import WelcomeContainer from './WelcomeContainer';
 import RpsContainer from './RpsContainer';
 import Rps2Container from './Rps2Container';
-import GameSelectContainer from './GameSelectContainer';
 
 
 const GameContainer = () => {
@@ -14,7 +13,6 @@ const GameContainer = () => {
       <Route exact path="/" component={WelcomeContainer} />
       <Route exact path="/rps" component={RpsContainer} />
       <Route exact path="/rps2" component={Rps2Container} />
-      <Route exact path="/select" component={GameSelectContainer} />
     </Switch>
     </Fragment>
     </Router>
